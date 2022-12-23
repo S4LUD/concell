b57ff77dcc9abfbf234e8b72a650a15b6c044522
S4LUD <salud.lanceivan@gmail.com>
2022-12-23 18:23:38 +0800

    Concell App Backend

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const routes = require(".Routes");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
