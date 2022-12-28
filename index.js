0c78bf33a4461b6bb1cd0574ba9fba97eddb0711
S4LUD <salud.lanceivan@gmail.com>
2022-12-28 19:37:25 +0800

    Concell App Backend

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ mongoose
   .then(() => console.log("Connected to mongoDB"))
   .catch((err) => console.log(err));
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 
 app.use("/api/concell", routes);
 
