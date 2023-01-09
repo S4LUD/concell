ff2a7bc181d8115226c5c0c4826365e866d5296c
S4LUD <salud.lanceivan@gmail.com>
2023-01-09 11:57:53 +0800

    Concell App Backend

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -397,7 +397,7 @@ router.post(
 );
 
 router.post("/room", accountVerificationMiddleware, (req, res) => {
-  roomModel.findOne({ creator_id: req.body._id }, (err, data) => {
+  roomModel.find({ creator_id: req.body._id }, (err, data) => {
     if (err) return res.status(400).send(err);
     res.status(200).send(data);
   });
